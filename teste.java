1c85f8773aa51c62b0282f028c2e64306feef33a
Luis <luiseduardo@ifsul.edu.br>
2024-08-02 20:24:06 -0300

    nova linha

--- teste.java
+++ teste.java
@@ -3,5 +3,6 @@ package ProgramacaoMobile1;
 public class teste {
     public static void main(String[] args){
         System.out.println("Olá Mundo!");
+        System.out.println("Olá Mundo!");
     }
 }
