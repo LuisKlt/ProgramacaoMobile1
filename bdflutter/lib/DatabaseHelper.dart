1bc2c7588a915e45d474e654f1b892390c9cad39
Luis <luis@gmail.com>
2024-10-04 20:36:43 -0300

    faltou um arquivo

--- bdflutter/lib/DatabaseHelper.dart
+++ bdflutter/lib/DatabaseHelper.dart
@@ -10,4 +10,27 @@ class Databasehelper {
   }
   Databasehelper._internal();
 
+  Future<Database> get database async{
+    if (_database != null) return _database!;
+    _database = await _initDatabase(); return _database!;
+  }
+
+  Future<Database> _initDatabase() async{
+    String dbPath = await getDatabasesPath();
+    return openDatabase(
+      join(dbPath, 'queroirembora.db'),
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async{
+    await db.execute('create table pessoa(id integer primary key autoincrement, nome text, cpf text, cartao text)');
+  }
+
+  Future close() async{
+    final db = await database;
+    db.close();
+  }
+
 }
